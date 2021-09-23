33064c6850fa3932018507df825681e6db2b66a9
huckiyang <huckiyang@gmail.com>
2021-09-23 00:47:04 -0400

    Update v2s_main.py

--- v2s_main.py
+++ v2s_main.py
@@ -1,3 +1,7 @@
+# CHH Yang et al. 2021 (http://proceedings.mlr.press/v139/yang21j/yang21j.pdf)
+# Apache Apache-2.0 License
+
+
 from tensorflow.keras.layers import Dense, ZeroPadding1D, Reshape
 import tensorflow as tf
 from tensorflow import keras
@@ -14,11 +18,14 @@ K.clear_session()
 # K.set_learning_phase(0)
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--mod", type = int, default = 0, help = "Single input seq (0), multiple input aug (1), repro w/ TF (2)")
 parser.add_argument("--net", type = int, default = 0, help = "Pretrained (0), AttRNN (#32), (1) VGGish (#512)")
 parser.add_argument("--dataset", type = int, default = 0, help = "Ford-A (0), Beef (1), ECG200 (2), Wine (3), Earthquakes (4), Worms (5), Distal (6), Outline Correct (7), ECG-5k (8), ArrowH (9), CBF (10), ChlorineCon (11)")
 parser.add_argument("--mapping", type= int, default=1, help = "number of multi-mapping")
 parser.add_argument("--eps", type = int, default = 100, help = "Epochs") 
 parser.add_argument("--per", type = int, default = 0, help = "save weight per N epochs")
+parser.add_argument("--dr", type=int, default = 4, help = "drop out rate")
+parser.add_argument("--seg", type=int, default = 1, help = "seg padding number")
 args = parser.parse_args()
 
 
@@ -28,6 +35,10 @@ classes = np.unique(np.concatenate((y_train, y_test), axis=0))
 
 x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], 1))
 x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))
+
+# Note for cross validation to report a new time series results. Please refer to the cross-validation and "only use the training loss" to tune the model.
+# (Dau et al., 2019) and (Wang et al., 2017) cited in the V2S paper
+# The x_test and y_test are the official validation set in the UCR.
 
 num_classes = len(np.unique(y_train))
 
@@ -48,28 +59,41 @@ print("--- X shape : ", x_train[0].shape, "--- Num of Classes : ", num_classes) 
 ## Pre-trained Model for Adv Program  
 if args.net == 0:
     pr_model = AttRNN_Model()
-else:
+elif args.net == 1:
     pr_model = Vggish_Model()
+elif args.net == 2: # audio-set
+    pr_model = VGGish_Model(audioset = True)
+elif args.net == 3: # unet
+    pr_model = AttRNN_Model(unet= True)
+
 
 # pr_model.summary()
 
 ## # of Source classes in Pre-trained Model
 if args.net == 0: ## choose pre-trained network 
     source_classes = 36 ## Google Speech Commands
+elif args.net == 2:
+    source_classes = 128 ## AudioSet by VGGish
 else:
-    source_classes = 512 ## VGGish
+    source_classes = 512 ## VGGish feature num
 
 target_shape = x_train[0].shape
 
 ## Adv Program Time Series (ART)
 mapping_num = args.mapping
+seg_num = args.seg
+drop_rate = args.dr*0.1
+
 try:
     assert mapping_num*num_classes <= source_classes
 except AssertionError:
     print("Error: The mapping num should be smaller than source_classes / num_classes: {}".format(source_classes//num_classes)) 
     exit(1)
 
-model = WARTmodel(target_shape, pr_model, source_classes, mapping_num, num_classes)
+model = WARTmodel(target_shape, pr_model, source_classes, mapping_num, num_classes, args.mod, seg_num, drop_rate)
+# else:
+# model = pr_model # define for transfer learning
+
 
 ## Loss
 adam = tf.keras.optimizers.Adam(lr=0.05,decay=0.48)
@@ -89,8 +113,12 @@ batch_size = 32
 epochs = args.eps
 
 # convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+if args.mod == 0: # single input w/ random mapping
+    y_train = keras.utils.to_categorical(y_train, source_classes)
+    y_test = keras.utils.to_categorical(y_test, source_classes)
+else: # with many to one mapping
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
 
 
 exp_history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1,
